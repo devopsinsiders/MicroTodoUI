ccf42edb7cd0b9387ac28f31cffe28272a9a5e30
DevOps Insiders <info@devopsinsiders.com>
2023-12-16 12:22:22 +0530

    update code

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -5,9 +5,9 @@ import { Delete } from '@mui/icons-material';
 import { Box } from '@mui/material';
 
 // Please update the below microservice URL's.
-const GET_TASKS_API_BASE_URL = 'http://get-tasks';
-const DELETE_TASK_API_BASE_URL = 'http://delete-task';
-const CREATE_TASK_API_BASE_URL = 'http://add-task';
+const GET_TASKS_API_BASE_URL = 'http://get-tasks.devopsinsiders.online';
+const DELETE_TASK_API_BASE_URL = 'http://delete-task.devopsinsiders.online';
+const CREATE_TASK_API_BASE_URL = 'http://add-task.devopsinsiders.online';
 
 //Update Task Functionality is Work In Progress
 // const UPDATE_TASK_API_BASE_URL = '';
